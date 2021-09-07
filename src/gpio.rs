fe3f647050087e5086233b24476839c26eef3753
Jordan Yelloz <jordan@yelloz.me>
2021-09-07 12:02:50 -0700

    gpio: Added all routed Tomu pins.
    Also, used the paste macro to reduce the amount of input data necessary
    to generate the code. Only the bank (a, b, c, ...), index (0..15), and
    half (low, high) are needed for now. Hopefully there's a simple way to
    get rid of the half part since it's basically an expression of the
    index.
    
    Also, corrected the default state for all pins which is Input<Disabled>
    and added mode switchers to Input<Disabled> and Output<OpenDrain>.

--- src/gpio.rs
+++ src/gpio.rs
@@ -3,6 +3,8 @@ use core::{
     marker::PhantomData,
 };
 
+use paste::paste;
+
 use crate::pac;
 
 pub use crate::hal::digital::v2::OutputPin;
@@ -13,76 +15,108 @@ pub trait GpioExt {
     fn split(self) -> Self::Parts;
 }
 
-pub struct Floating;
-pub struct PullDown;
-pub struct PullUp;
-pub struct OpenDrain;
+pub struct Disabled;
 pub struct PushPull;
+pub struct WiredAnd;
 
-pub struct Input<MODE> {
-    _mode: PhantomData<MODE>,
-}
+pub type OpenDrain = WiredAnd;
 
-pub struct Output<MODE> {
-    _mode: PhantomData<MODE>,
-}
+pub struct Input<MODE>(PhantomData<MODE>);
 
-#[derive(Debug, PartialEq)]
-pub enum Edge {
-    Rising,
-    Falling,
-    RisingFalling,
-}
+pub struct Output<MODE>(PhantomData<MODE>);
 
 macro_rules! gpio {
     ($gpiox:ident, $PXx:ident, [
-        $($PXi:ident: ($pxi:ident, $i:expr, $MODE:ty, $set:ident, $clr:ident),)+
+        $($PXi:ident: ($bank:ident, $i:expr, $mode_part:ident),)+
     ]) => {
-        pub struct Parts {
+        paste! {
+            pub struct Parts {
+                $(
+                    pub [<$PXi:lower>]: $PXi<Input<Disabled>>,
+                )+
+            }
+
+            impl GpioExt for pac::GPIO {
+                type Parts = Parts;
+
+                fn split(self) -> Self::Parts {
+                    Self::Parts {
+                        $(
+                            [<$PXi:lower>]: $PXi(PhantomData),
+                        )+
+                    }
+                }
+            }
+
             $(
-                pub $pxi: $PXi<$MODE>,
+                pub struct $PXi<MODE>(PhantomData<MODE>);
+
+                #[allow(dead_code)]
+                impl <MODE> $PXi<MODE> {
+                    pub fn into_disabled_input(self) -> $PXi<Input<Disabled>> {
+                        #[allow(unsafe_code)]
+                        let gpio = unsafe { &*pac::GPIO::ptr() };
+                        paste! {
+                            gpio.[<p $bank _mode $mode_part>]
+                                .modify(|_, w| w.[<mode $i>]().disabled());
+                        }
+                        $PXi(PhantomData)
+                    }
+                    pub fn into_push_pull_output(self) -> $PXi<Output<PushPull>> {
+                        #[allow(unsafe_code)]
+                        let gpio = unsafe { &*pac::GPIO::ptr() };
+                        paste! {
+                            gpio.[<p $bank _mode $mode_part>]
+                                .modify(|_, w| w.[<mode $i>]().pushpull());
+                        }
+                        $PXi(PhantomData)
+                    }
+                    pub fn into_open_drain_output(self) -> $PXi<Output<WiredAnd>> {
+                        #[allow(unsafe_code)]
+                        let gpio = unsafe { &*pac::GPIO::ptr() };
+                        paste! {
+                            gpio.[<p $bank _mode $mode_part>]
+                                .modify(|_, w| w.[<mode $i>]().wiredand());
+                        }
+                        $PXi(PhantomData)
+                    }
+                }
+                impl <MODE> OutputPin for $PXi<Output<MODE>> {
+                    type Error = Infallible;
+                    fn set_low(&mut self) -> Result<(), Self::Error> {
+                        #[allow(unsafe_code)]
+                        unsafe {
+                            (*pac::GPIO::ptr()).[<p $bank _doutset>].write(|w| w.bits(1 << $i))
+                        };
+                        Ok(())
+                    }
+                    fn set_high(&mut self) -> Result<(), Self::Error> {
+                        #[allow(unsafe_code)]
+                        unsafe {
+                            (*pac::GPIO::ptr()).[<p $bank _doutclr>].write(|w| w.bits(1 << $i))
+                        };
+                        Ok(())
+                    }
+                }
             )+
         }
-
-        impl GpioExt for pac::GPIO {
-            type Parts = Parts;
-
-            fn split(self) -> Self::Parts {
-                Self::Parts {
-                    $(
-                        $pxi: $PXi { _mode: PhantomData },
-                    )+
-                }
-            }
-        }
-
-        $(
-            pub struct $PXi<MODE> {
-                _mode: PhantomData<MODE>,
-            }
-            impl <MODE> OutputPin for $PXi<Output<MODE>> {
-                type Error = Infallible;
-                fn set_low(&mut self) -> Result<(), Self::Error> {
-                    #[allow(unsafe_code)]
-                    unsafe {
-                        (*pac::GPIO::ptr()).$set.write(|w| w.bits(1 << $i))
-                    };
-                    Ok(())
-                }
-                fn set_high(&mut self) -> Result<(), Self::Error> {
-                    #[allow(unsafe_code)]
-                    unsafe {
-                        (*pac::GPIO::ptr()).$clr.write(|w| w.bits(1 << $i))
-                    };
-                    Ok(())
-                }
-            }
-
-        )+
     }
 }
 
+// PA0/PB7 = Green/Red Light
+// PC0/PC1 = Cap0/1B
+// PC14/15 = USB +/-
+// PE12/13 = Cap0/1A
 gpio!(gpio, PXx, [
-    PA0: (pa0, 0, Output<PushPull>, pa_doutset, pa_doutclr),
-    PB7: (pb7, 7, Output<PushPull>, pb_doutset, pb_doutclr),
+    PA0: (a, 0, l),
+    PB7: (b, 7, l),
+
+    PC0: (c, 0, l),
+    PC1: (c, 1, l),
+
+    PC14: (c, 14, h),
+    PC15: (c, 15, h),
+
+    PE12: (e, 12, h),
+    PE13: (e, 13, h),
 ]);
